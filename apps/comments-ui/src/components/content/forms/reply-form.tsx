7bf60792f19654f696b866fb7a8015729b078975
ofir-frd <ofir.f@qodo.ai>
2026-01-21 10:21:19 +0200

    update pr

--- apps/comments-ui/src/components/content/forms/reply-form.tsx
+++ apps/comments-ui/src/components/content/forms/reply-form.tsx
@@ -44,7 +44,7 @@ const ReplyForm: React.FC<Props> = ({openForm, parent}) => {
 
     return (
         <div ref={setForm} data-testid="reply-form">
-            <div className='mt-[-16px] pr-2'>
+            <div className='pr-2 mt-[-16px]'>
                 <FormWrapper editor={editor} isOpen={true} openForm={openForm} reduced={isMobile()}>
                     <Form
                         close={close}
